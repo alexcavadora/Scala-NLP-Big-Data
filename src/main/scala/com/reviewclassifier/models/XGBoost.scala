8c4aaa285104a587e844a93de41e63bcab0bcbdb
Emilio Muñoz Rivera <123415921+Emili01@users.noreply.github.com>
2025-10-17 15:18:52 -0600

    XGBoost Model
    XGBoost Model version 1

---
+++ src/main/scala/com/reviewclassifier/models/XGBoost.scala
@@ -0,0 +1,47 @@
+package com.reviewclassifier.models
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.ml.{Pipeline, Transformer}
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import ml.dmlc.xgboost4j.scala.spark.XGBoostClassifier
+import org.apache.spark.sql.functions.udf
+import com.reviewclassifier.Config
+
+class XGBoostModel(config: Config) extends BaseModel {
+
+  override def train(data: DataFrame): Transformer = {
+    val classWeights = calculateClassWeights(data)
+    val labelToWeightUDF = udf((label: Double) => classWeights.getOrElse(label, 1.0))
+    val dataWithWeights = data.withColumn("class_weight", labelToWeightUDF($"sentiment_label"))
+    val trainingData = dataWithWeights.select("features", "sentiment_label", "class_weight")
+
+    val xgboost = new XGBoostClassifier()
+      .setFeaturesCol("features")
+      .setLabelCol("sentiment_label")
+      .setWeightCol("class_weight")
+      .setEta(config.eta)
+      .setMaxDepth(config.maxDepth)
+      .setObjective("multi:softprob")
+      .setNumClass(config.numClasses)
+      .setNumRound(config.numRound)
+      .setNumWorkers(config.numWorkers)
+      .setSeed(config.seed)
+
+    val pipeline = new Pipeline().setStages(Array(xgboost))
+    pipeline.fit(trainingData)
+  }
+
+  private def calculateClassWeights(data: DataFrame): Map[Double, Double] = {
+    val classCounts = data.groupBy("sentiment_label").count().collect()
+    val totalSamples = data.count().toDouble
+    val numClasses = config.numClasses.toDouble
+
+    classCounts.map { row =>
+      val label = row.getAs[Double]("sentiment_label")
+      val count = row.getAs[Long]("count").toDouble
+      val weight = totalSamples / (numClasses * count)
+      (label, weight)
+    }.toMap
+  }
+
+  override def getName: String = "XGBoost"
+}
